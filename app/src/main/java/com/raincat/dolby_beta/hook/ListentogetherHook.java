0aad095006f5b8ee9db9fb95b43a27bfb3924ebe
luoxingran <szb5845201314@gmail.com>
2022-07-07 10:14:49 +0800

    提升一起听解锁版本范围8.6.0-8.7.90

--- app/src/main/java/com/raincat/dolby_beta/hook/ListentogetherHook.java
+++ app/src/main/java/com/raincat/dolby_beta/hook/ListentogetherHook.java
@@ -10,7 +10,10 @@ import static de.robv.android.xposed.XposedHelpers.findClass;
 
 public class ListentogetherHook {
     public ListentogetherHook(Context context, int versionCode) {
-        if (versionCode > 8007075) {
+        if (versionCode > 8007090) {
+            findAndHookMethod(findClass("com.netease.cloudmusic.module.listentogether.f2", context.getClassLoader()),
+                    "v", XC_MethodReplacement.returnConstant(true));
+        }else if (versionCode > 8007075) {
             findAndHookMethod(findClass("com.netease.cloudmusic.module.listentogether.x", context.getClassLoader()),
                     "v", XC_MethodReplacement.returnConstant(true));
         }else if (versionCode > 8007070) {
