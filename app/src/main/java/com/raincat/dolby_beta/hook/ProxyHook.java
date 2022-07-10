9a2a532010762a369ae425d6cd45e314b95156fb
nining377 <nining377@gmail.com>
2022-07-10 09:42:21 +0800

    merge

--- app/src/main/java/com/raincat/dolby_beta/hook/ProxyHook.java
+++ app/src/main/java/com/raincat/dolby_beta/hook/ProxyHook.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import com.raincat.dolby_beta.helper.ExtraHelper;
 import com.raincat.dolby_beta.helper.ScriptHelper;
 import com.raincat.dolby_beta.helper.SettingHelper;
-import com.raincat.dolby_beta.utils.Tools;
 
 import java.lang.reflect.Field;
 import java.net.InetSocketAddress;
@@ -17,6 +16,8 @@ import java.util.List;
 import javax.net.ssl.SSLSocketFactory;
 
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 
 import static de.robv.android.xposed.XposedBridge.hookAllConstructors;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
@@ -40,17 +41,10 @@ public class ProxyHook {
     private String fieldSSLSocketFactory;
     private String fieldHttpUrl = "url";
     private String fieldProxy = "proxy";
-    private String HttpUrlhost = "127.0.0.1";
-
 
     private final List<String> whiteUrlList = Arrays.asList("song/enhance/player/url", "song/enhance/download/url");
 
-    public ProxyHook(Context context, int versionCode, boolean isPlayProcess) {
-        if (!SettingHelper.getInstance().isEnable(SettingHelper.proxy_master_key)) {
-            ExtraHelper.setExtraDate(ExtraHelper.SCRIPT_STATUS, "0");
-            return;
-        }
-
+    public ProxyHook(Context context, boolean isPlayProcess) {
         Class<?> realCallClass = findClassIfExists("okhttp3.internal.connection.RealCall", context.getClassLoader());
         if (realCallClass != null) {
             fieldSSLSocketFactory = "sslSocketFactoryOrNull";
@@ -75,9 +69,7 @@ public class ProxyHook {
 
                     Field urlField = request.getClass().getDeclaredField(fieldHttpUrl);
                     urlField.setAccessible(true);
-
                     Object urlObj = urlField.get(request);
-
                     for (String url : whiteUrlList) {
                         if (urlObj.toString().contains(url)) {
                             setProxy(context, client);
@@ -88,34 +80,45 @@ public class ProxyHook {
             }
         });
 
-        if (isPlayProcess)
-            findAndHookMethod("com.netease.cloudmusic.service.PlayService", context.getClassLoader(), "onCreate", new XC_MethodHook() {
+        Class<?> okHttpClientBuilderClass = XposedHelpers.findClassIfExists("okhttp3.OkHttpClient$Builder", context.getClassLoader());
+        if (okHttpClientBuilderClass != null) {
+            XposedBridge.hookAllMethods(okHttpClientBuilderClass, "addInterceptor", new XC_MethodHook() {
                 @Override
-                protected void afterHookedMethod(MethodHookParam param) {
-                    int retry = Integer.parseInt(ExtraHelper.getExtraDate(ExtraHelper.SCRIPT_RETRY));
-                    if (retry > 0) {
-                        ScriptHelper.initScript(context, retry == 1);
-                        if (SettingHelper.getInstance().getSetting(SettingHelper.proxy_server_key)) {
-                            ScriptHelper.starhttptproxyMode(context);
-                        }
-                        else{
-                            if (SettingHelper.getInstance().getSetting(SettingHelper.proxy_compatibility_key))
-                                ScriptHelper.startScriptCompatibilityMode(context);
-                            else
-                                ScriptHelper.startScript(context);
-                        }
-                        ExtraHelper.setExtraDate(ExtraHelper.SCRIPT_RETRY, --retry);
-                    } else
-                        Tools.showToastOnLooper(context, "重试次数过多，UnblockNeteaseMusic运行失败！");
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    super.beforeHookedMethod(param);
+                    if (param.args[0].getClass().getName().contains("com.netease.cloudmusic.network.cronet"))
+                        param.setResult(param.thisObject);
+//                        XposedBridge.hookAllMethods(param.args[0].getClass(), "intercept", new XC_MethodHook() {
+//                            @Override
+//                            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+//                                super.beforeHookedMethod(param);
+//                                Object object = param.args[0];
+//                                if (object != null && object.getClass().getName().contains("Chain")) {
+//                                    Object request = XposedHelpers.callMethod(object, "request");
+//                                    if (request.toString().contains("song/enhance/player/url") || request.toString().contains("song/enhance/download/url")) {
+//                                        Object response = XposedHelpers.callMethod(object, "proceed", request);
+//                                        param.setResult(response);
+//                                    }
+//                                }
+//                            }
+//                        });
                 }
             });
+        }
 
         if (!isPlayProcess)
             findAndHookMethod("com.netease.cloudmusic.activity.LoadingActivity", context.getClassLoader(), "onCreate", Bundle.class, new XC_MethodHook() {
                 @Override
                 protected void afterHookedMethod(MethodHookParam param) {
                     ExtraHelper.setExtraDate(ExtraHelper.SCRIPT_STATUS, "0");
-                    ExtraHelper.setExtraDate(ExtraHelper.SCRIPT_RETRY, "3");
+                    if (SettingHelper.getInstance().getSetting(SettingHelper.proxy_master_key)) {
+                        ScriptHelper.initScript(context, false);
+                        if (SettingHelper.getInstance().getSetting(SettingHelper.proxy_server_key)) {
+                            ScriptHelper.startHttpProxyMode(context);
+                        } else {
+                            ScriptHelper.startScript();
+                        }
+                    }
                 }
             });
     }
@@ -124,23 +127,20 @@ public class ProxyHook {
      * 设置代理
      */
     private void setProxy(Context context, Object client) throws Exception {
-        if (SettingHelper.getInstance().getSetting(SettingHelper.proxy_server_key)) {
-            HttpUrlhost = SettingHelper.getInstance().gethttpProxy();
-        }
-        else{
-            HttpUrlhost="127.0.0.1";
-        }
-        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(HttpUrlhost, SettingHelper.getInstance().getProxyPort()));
-        Field proxyField = client.getClass().getDeclaredField(fieldProxy);
-        proxyField.setAccessible(true);
+        //保存正常的代理与SSL
         Field sslSocketFactoryField = client.getClass().getDeclaredField(fieldSSLSocketFactory);
         sslSocketFactoryField.setAccessible(true);
+        Field proxyField = client.getClass().getDeclaredField(fieldProxy);
+        proxyField.setAccessible(true);
         if (objectProxy == null)
             objectProxy = proxyField.get(client);
         if (objectSSLSocketFactory == null)
             objectSSLSocketFactory = sslSocketFactoryField.get(client);
 
         if (ExtraHelper.getExtraDate(ExtraHelper.SCRIPT_STATUS).equals("1")) {
+            String httpUrlHost = SettingHelper.getInstance().getSetting(SettingHelper.proxy_server_key) ?
+                    SettingHelper.getInstance().getHttpProxy() : "127.0.0.1";
+            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(httpUrlHost, SettingHelper.getInstance().getProxyPort()));
             proxyField.set(client, proxy);
             if (socketFactory == null)
                 socketFactory = ScriptHelper.getSSLSocketFactory(context);
